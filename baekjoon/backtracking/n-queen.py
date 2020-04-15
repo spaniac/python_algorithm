66e8b9ef0d4d0d5b3fa2db85ff3c0d3b434f663d
박세준 <spaniac2@gmail.com>
2020-04-16 04:02:48 +0900

    n-queen 풀이 진행 중

--- baekjoon/backtracking/n-queen.py
+++ baekjoon/backtracking/n-queen.py
@@ -1,31 +1,78 @@
 import sys
 
+# def solution():
+#     input = sys.stdin.readline
+#     n = 1
+#     result = 0
+#     start = 0
+#     while start < n:
+#         queue = deque([start])
+#
+#         if len(queue) == n:
+#             result += 1
+#         else:
+#             for nc in range(n):
+#                 queue.append(nc)
+#                 if is_possible(pos, nc):
+#                     queue.append(pos + [nc])
+#         start += 1
+#     print(result)
+#
+#
+# def is_possible(pos, nc):
+#     if nc in pos:
+#         return False
+#     nr = len(pos)
+#     for r, c in enumerate(pos):
+#         if abs(nr - r) == abs(nc - c):
+#             return False
+#     return True
 
-def solution():
+
+result = 0
+n = None
+col = [-1] * 15
+c = None
+
+def solution2():
     input = sys.stdin.readline
-    n = int(input())
-    result = 0
-    for start in range(n):
-        queue = [[(0, start)]]
-        while queue:
-            locs = queue.pop()
-            if len(locs) < n:
-                for nc in range(n):
-                    if is_possible(locs, nc):
-                        queue.append(locs + [(locs[-1][0] + 1, nc)])
-            else:
-                result += 1
+    # n = int(input())
+    global n
+    n = 12
+    global col
+    global result
+    global c
+    c = range(n)
+    for i in c:
+        col[0] = i
+        dfs(0)
     print(result)
 
 
-def is_possible(locs, nc):
-    imp_cols = map(lambda x: x[1], locs)
-    if nc in imp_cols:
+def dfs(row):
+    global n
+    global result
+    global col
+    global c
+    if row == n - 1:
+        result += 1
+    else:
+        for i in c:
+            col[row + 1] = i
+            if is_possible2(row + 1):
+                dfs(row + 1)
+            else:
+                col[row + 1] = -1
+    col[row] = -1
+
+
+def is_possible2(row):
+    global col
+    if col[row] in col[:row]:
         return False
-    next_row = locs[-1][0] + 1
-    imp_diag = map(lambda x: abs(next_row - x[0]) - abs(nc - x[1]), locs)
-    if 0 in imp_diag:
-        return False
+    for i in range(row):
+        if abs(row - i) == abs(col[row] - col[i]):
+            return False
     return True
 
 
@@ -40,4 +87,5 @@ class TestSolution(unittest.TestCase):
         pass
 
     def test_solution(self):
-        solution()
+        # solution()
+        solution2()
